31adbbf2cef9543ba69f4ed79bd649afaa527739
mannsvijay <mnvj69@gmail.com>
2026-07-06 14:16:22 +0530

    fix Vehicles class to use string for name and enhance Car constructor with TransmissionType initialization

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Vehicles {
 public :
-    int name;
+    string name;
     string model;
     int noOfTyres;
     
@@ -11,9 +11,15 @@ public :
         cout<<"vehicles constructor called"<<endl;
     }
 
+    Vehicles(string _name, string _model, int _noOfTyres) {
+        name = _name;
+        model = _model;
+        noOfTyres = _noOfTyres;
+        cout<<"vehicles constructor called"<<endl;
+    }
+
     void start_engine() {
         cout<<"engine started"<<endl;
-
     }
 
     void stop_engine() {
@@ -33,6 +39,16 @@ public :
     Car() {
         cout<<"Car constructor called"<<endl;
     }
+
+    // we can also call the base class constructor from the derived class constructor using the initializer list so that we can initialize the base class members as well
+    // and we dont have to call the base class constructor explicitly in the derived class constructor body
+    Car(string _name, string _model, int _noOfTyres, int _noOfDoors, string _TransmissionType) : Vehicles(_name, _model, _noOfTyres) {
+        noOfDoors = _noOfDoors;
+        TransmissionType = _TransmissionType;
+        cout<<"Car constructor called"<<endl;
+    }
+
+
     void startAC() {
         cout<<"AC started"<<endl;
     }
