9c4aadd19d9c51542219dc85873b2936efa79ff1
mannsvijay <mnvj69@gmail.com>
2026-07-11 23:53:21 +0530

    refactor Bird class and main function for clarity and correctness

--- oops/abstraction/bird.h
+++ oops/abstraction/bird.h
@@ -1,48 +1,85 @@
 #if !defined(BIRD_H)
 #define BIRD_H
 #include <bits/stdc++.h>
+
+// =========================================================
+// ABSTRACTION IN SIMPLE WORDS
+// =========================================================
+// Abstraction means showing only the important things and
+// hiding the unnecessary details.
+//
+// Real-life example:
+// When you drive a car, you use the steering wheel, brake,
+// and accelerator. You do not need to know how the engine
+// works internally. That is abstraction.
+//
+// In OOP, abstraction helps us create a simple interface
+// for a concept and let different classes implement it in their own way.
+//
+// Here, Bird is an abstract class.
+// It defines what a bird should be able to do:
+// - eat()
+// - fly()
+//
+// But it does not define how each bird does it.
+// Each bird type (Sparrow, Eagle, Pigeon) gives its own implementation.
+//
+// Why use abstraction?
+// - To focus on what is important
+// - To avoid showing unnecessary internal details
+// - To create a common structure for related classes
+// - To make code easier to extend and maintain
+// =========================================================
+
 class Bird
 {
 public:
-    // this is interface
+    // This is an abstract interface.
+    // Pure virtual functions mean:
+    // "Every child class must provide its own version of this function."
     virtual void eat() = 0;
     virtual void fly() = 0;
-    //   classes that inherits this class has to implement pure virtual functions
 };
+
 class Sparrow : public Bird
 {
 public:
-    void eat()
+    void eat() override
     {
         std::cout << "Sparrow is eating\n";
     }
-    void fly()
+
+    void fly() override
     {
-        std::cout << "Sparrow is flying \n";
+        std::cout << "Sparrow is flying\n";
     }
 };
+
 class Eagle : public Bird
 {
 public:
-    void eat()
+    void eat() override
     {
         std::cout << "Eagle is eating\n";
     }
-    void fly()
+
+    void fly() override
     {
-        std::cout << "Eagle is flying \n";
+        std::cout << "Eagle is flying\n";
     }
 };
-class Pigeion : public Bird
+
+class Pigeon : public Bird
 {
 public:
-    void eat()
+    void eat() override
     {
-        std::cout << "Pigeion is eating\n";
+        std::cout << "Pigeon is eating\n";
     }
-    void fly()
+
+    void fly() override
     {
-        std::cout << "Pigeion is flying \n";
+        std::cout << "Pigeon is flying\n";
     }
 };
 #endif
--- oops/abstraction/main.cpp
+++ oops/abstraction/main.cpp
@@ -1,17 +1,33 @@
 #include <bits/stdc++.h>
 #include "bird.h"
 using namespace std;
-void birdDoesSomething(Bird *&bird)
+
+// This function uses abstraction.
+// We don't care whether the object is a Sparrow, Eagle, or Pigeon.
+// We only care that it is a Bird and can eat() and fly().
+void birdDoesSomething(Bird *bird)
 {
     bird->eat();
     bird->fly();
 }
+
 int main()
 {
+    // We create different bird objects.
+    // The pointer type is Bird*, but the actual object can be any child class.
+
     // Bird *bird = new Sparrow();
     // Bird *bird = new Eagle();
-    Bird *bird = new Pigeion();
+    Bird *bird = new Pigeon();
+
     birdDoesSomething(bird);
-    // interface same h but functionality alag ho skti h ya changes ho skte h humme code mai koi change nhi krna
+
+    // Why is this useful?
+    // Because the same function birdDoesSomething() works for all birds.
+    // We do not need to write separate code for Sparrow, Eagle, or Pigeon.
+    // This is abstraction in action:
+    // we use a common interface (Bird) and hide the specific details.
+
+    delete bird;
     return 0;
 }
